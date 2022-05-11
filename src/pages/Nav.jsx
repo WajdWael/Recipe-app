d957cd5b9d268eb9a3430622c2fc2395fa720293
WajdWael <wajdwael@gmail.com>
2022-05-12 01:03:33 +0300

    edit logo

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -11,7 +11,7 @@ export default function Nav() {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
         >
-            <GiKnifeFork />
+            <GiKnifeFork style={{ fontSize: '3rem'}}/>
             <Logo to={"/"}>deliciousss</Logo>
         </NavStyles>
     )
@@ -19,7 +19,7 @@ export default function Nav() {
 
 const Logo = styled(Link)`
     text-decoration: none;
-    font-size: 1.5rem;
+    font-size: 3.785rem;
     font-weight: 400;
     font-family: 'Lobster Two', cursive;
 `;
@@ -27,7 +27,7 @@ const Logo = styled(Link)`
 const NavStyles = styled(motion.div)`
     padding: 4rem 0rem;
     display: flex;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
 
     svg{
